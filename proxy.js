deb325fff40793a6ab4d54db78e39796d6cebf6a
tempurbi <249733004+tempurbi@users.noreply.github.com>
2025-12-15 11:20:06 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ proxy.js
@@ -0,0 +1,14 @@
+// api/proxy.js (Exemplo de código Node.js)
+module.exports = (req, res) => {
+  // 1. Pega os dados do Arduino (Query String)
+  const queryString = req.url.split('?')[1]; 
+
+  // 2. Monta o URL de destino HTTPS
+  const targetUrl = 'https://script.google.com/macros/.../exec?' + queryString;
+
+  // 3. Faz a requisição HTTPS para o Google
+  fetch(targetUrl)
+      .then(googleRes => googleRes.text())
+      .then(data => res.status(200).send(data)) // Retorna 200 ao Arduino
+      .catch(error => res.status(500).send('Proxy Error'));
+};
